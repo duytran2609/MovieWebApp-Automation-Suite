7a570c9ac53bc57a0b95c3f7702440c9ea75fabc
Duy <trandangduy13@gmail.com>
2026-01-09 11:25:58 +0700

    Added auto screenshot failed test

---
+++ src/main/java/utils/ScreenshotUtils.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.openqa.selenium.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ScreenshotUtils {
+
+    public static String takeScreenshot(WebDriver driver, String testName) {
+        try {
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+            String dir = "screenshots";
+            Files.createDirectories(Path.of(dir));
+            String path = dir + "/" + testName + "_" + time + ".png";
+            Files.copy(src.toPath(), Path.of(path));
+            return path;
+        } catch (Exception e) {
+            return "FAILED_TO_TAKE_SCREENSHOT";
+        }
+    }
+
+}
